96dc76a6d41f5520eb1f1570aaa671f4a71d8c98
FelipeESchmidt <schmidtfe2@gmail.com>
2021-10-01 02:05:29 -0300

    Communication with RSA workin and first AES trade

--- Client.py
+++ Client.py
@@ -30,17 +30,38 @@ key = get_random_bytes(16)
 cipher = AES.new(key, AES.MODE_EAX)
 nonce = cipher.nonce
 
-# Write line to server
+# Send my AES key to server
 clientSocket.sendall(rsa.encrypt(key, publicKeyServer))
+
+# Send my AES nonce to server
 clientSocket.sendall(rsa.encrypt(nonce, publicKeyServer))
 
+# Get message to send
 message = input('Client ready for input\n');
 
+# Encrypt message with AES
 ciphertext, tag = cipher.encrypt_and_digest(message.encode('utf-8'))
 
+# Send my AES message tag to server
 clientSocket.sendall(rsa.encrypt(tag, publicKeyServer))
+
+# Send my AES message to server
 clientSocket.sendall(rsa.encrypt(ciphertext, publicKeyServer))
 
+# Read line from server
+key = clientSocket.recv(1024)
+
+# Read line from server
+message = clientSocket.recv(1024)
+
+# Decrypt client Message with Client RSA key
+plaintext = cipher.decrypt(message)
+
+try:
+    cipher.verify(tag)
+    print("The message is authentic:", plaintext)
+except ValueError:
+    print("Key incorrect or message corrupted")
 
 # Close the socket
 clientSocket.close()
--- Server.py
+++ Server.py
@@ -30,25 +30,50 @@ while 1:
     else:
         connectionSocket.close()
 
-    # Read input line from socket
+    # Wait client Key and decrypting it
     clientKey = connectionSocket.recv(1024)
     clientKey = rsa.decrypt(clientKey,private)
+
+    # Wait client Nonce and decrypting it
     clientNonce = connectionSocket.recv(1024)
     clientNonce = rsa.decrypt(clientNonce,private)
 
+    # Mount AES client key
     clientCipher = AES.new(clientKey, AES.MODE_EAX, nonce=clientNonce)
 
+    # Wait client Tag and decrypting it
     clientTag = connectionSocket.recv(1024)
     clientTag = rsa.decrypt(clientTag,private)
+
+    # Wait client Message and decrypting it
     clientMessage = connectionSocket.recv(1024)
     clientMessage = rsa.decrypt(clientMessage,private)
 
-    
+    # Decrypt client Message with Client RSA key
     plaintext = clientCipher.decrypt(clientMessage)
+    plaintext = plaintext.decode('utf-8')
 
+    # Verify authentic
     try:
         clientCipher.verify(clientTag)
         print("The message is authentic:", plaintext)
+
+        # Capitalize the sentence
+        plaintextUpper = plaintext.upper()
+
+        print(plaintextUpper)
+
+        # Encrypt plaintextUpper with AES
+
+        nextAESkey = AES.AddRoundKey(clientCipher)
+
+        ciphertext, tag = nextAESkey.encrypt_and_digest(plaintextUpper.encode('utf-8'))
+
+        # Send the AES plaintextUpper tag to client
+        clientSocket.sendall(tag)
+
+        # Send the AES plaintextUpper to client
+        clientSocket.sendall(ciphertext)
     except ValueError:
         print("Key incorrect or message corrupted")
 
